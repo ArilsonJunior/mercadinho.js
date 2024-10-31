eec5f5488da5d4e3d680fe9bbc5f0e5aa6c130b5
Arilson <juniorarilson870@gmail.com>
2024-10-31 17:36:25 -0300

    mercadinhos simples

---
+++ mercado.js
@@ -0,0 +1,66 @@
+const prompt = require("prompt-sync")();
+
+console.log("\nbem vindo ao mercadinho")
+
+let arroz = 0;
+
+let feijao = 0;
+
+let macarrao = 0;
+
+let repositor = 0;
+
+let removedor = 0;
+
+let sair = false
+do {
+    console.log("----menu----\n" +
+        "[1] mostrar estoque\n" +
+        "[2] adicionar item no estoque\n" +
+        "[3] remover item do carrinho\n" +
+        "[4] sair");
+
+    let escolha = parseInt(prompt("Qual aba deseja abrir?: "));
+
+    switch (escolha) {
+        case 1:
+            console.log("Arroz: " + arroz + " unidades");
+            console.log("feijao:" + feijao + " unidades");
+            console.log("macarrão: " + macarrao + " unidades");
+
+            break;
+
+        case 2:
+            repositor = parseInt(prompt("Deseja adicionar quantos arroz?: "));
+            arroz += repositor;
+
+            repositor = parseInt(prompt("Deseja adicionar quantos Feijão?: "));
+            feijao += repositor;
+
+            repositor = parseInt(prompt("Deseja adicionar quantos macarrão?: "));
+            macarrao += repositor;
+
+            break;
+
+        case 3:
+            removedor = prompt("Deseja remover quantos arroz?: ");
+            arroz = arroz - removedor;
+
+            removedor = prompt("Deseja remover quantos Feijão?: ");
+            feijao = feijao - removedor;
+
+            removedor = prompt("Deseja remover quantos macarrão?: ");
+            macarrao = macarrao - removedor;
+            
+            break;
+
+        case 4:
+            sair = true;
+            break;
+
+        default:
+            console.log("Opção invalida")
+            break;
+    }
+
+} while (!sair)
